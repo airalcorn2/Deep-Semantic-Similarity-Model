94e4a3372663e26ce0f91147e7da878faf14da93
Michael A. Alcorn <airalcorn2@gmail.com>
2016-05-04 21:02:41 -0500

    Change N_GRAM_SIZE variable name to more accurate WINDOW_SIZE.

--- deep_semantic_similarity_keras.py
+++ deep_semantic_similarity_keras.py
@@ -26,9 +26,9 @@ def R(vects):
 
 
 LETTER_GRAM_SIZE = 3 # See section 3.2.
-N_GRAM_SIZE = 3 # See section 3.2.
+WINDOW_SIZE = 3 # See section 3.2.
 TOTAL_LETTER_GRAMS = int(3 * 1e4) # Determined from data. See section 3.2.
-WORD_DEPTH = N_GRAM_SIZE * TOTAL_LETTER_GRAMS # See equation (1).
+WORD_DEPTH = WINDOW_SIZE * TOTAL_LETTER_GRAMS # See equation (1).
 K = 300 # Dimensionality of the max-pooling layer. See section 3.4.
 L = 128 # Dimensionality of latent semantic space. See section 3.5.
 J = 4 # Number of random unclicked documents serving as negative examples for a query. See section 4.
